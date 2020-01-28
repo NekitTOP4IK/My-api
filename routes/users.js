0a8bc3a9eaf5bfa97ab0768b0301fca2f37f58b9
Anton Kornilkov <anton-kornilkov@yandex.ru>
2020-01-28 21:12:04 +0500

    Development started

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {name:"Nikita", age: 13},
+  {name:"Anton", age: 27},
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/searchbyname', function(req, res, next) {
+  var name = req.query.name;
+  for (i=0;i<users.length;i++)
+  {
+    if (users[i].name===name) {
+      res.send(users[i]);
+      return
+    }
+  }
+  res.send(`User Not Found`);
 });
 
 module.exports = router;
